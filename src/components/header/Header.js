ec4241ce203d8d7fd1792fce36b0aa5a3bad0f99
Brianna Workman <brianna.workman@hotmail.com>
2020-08-29 17:37:38 -0400

    sets up and renders description

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,16 @@
 import React from 'react';
+
+// component imports
 import Heading from '../heading/Heading';
+import Paragraph from '../paragraph/Paragraph';
+
 import './header.scss';
 
 function Header() {
   return (
     <div className='header-container'>
       <Heading />
+      <Paragraph />
     </div>
   );
 }
---
+++ src/components/paragraph/Paragraph.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './paragraph.scss';
+
+function Paragraph() {
+  return (
+    <p className='heading-paragraph'>
+      I have a passion for building exceptional, high-quality websites and
+      applications for the web, iOS, and Android.
+    </p>
+  );
+}
+
+export default Paragraph;
