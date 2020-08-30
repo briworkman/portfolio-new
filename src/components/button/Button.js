d6a7a5d44b0cab210aeea9dd46870a95f4bf343b
Brianna Workman <brianna.workman@hotmail.com>
2020-08-30 12:59:51 -0400

    moves snackbar styling into scss file and removes styled components

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -2,66 +2,6 @@ import React, { useEffect, useState } from 'react';
 import './button.scss';
 import resume from '../../assets/Brianna_Workman_Resume.pdf';
 
-import styled from 'styled-components';
-
-const Snackbar = styled('div')`
-  min-width: 20vw;
-  background-color: #ffff;
-  color: #000;
-  text-align: center;
-  border-radius: 2px;
-  padding: 20px;
-  position: fixed;
-  z-index: 1;
-  left: 95%;
-  transform: translateX(-100%);
-  bottom: 2vh;
-  font-size: 1rem;
-  line-height: 1.5rem;
-  -webkit-animation: fadein 0.5s, fadeout 0.5s 2.5s;
-  animation: fadein 0.5s, fadeout 0.5s 2.5s;
-  @-webkit-keyframes fadein {
-    from {
-      bottom: 0;
-      opacity: 0;
-    }
-    to {
-      bottom: 2vh;
-      opacity: 1;
-    }
-  }
-  @keyframes fadein {
-    from {
-      bottom: 0;
-      opacity: 0;
-    }
-    to {
-      bottom: 2vh;
-      opacity: 1;
-    }
-  }
-  @-webkit-keyframes fadeout {
-    from {
-      bottom: 2vh;
-      opacity: 1;
-    }
-    to {
-      bottom: 0;
-      opacity: 0;
-    }
-  }
-  @keyframes fadeout {
-    from {
-      bottom: 2vh;
-      opacity: 1;
-    }
-    to {
-      bottom: 0;
-      opacity: 0;
-    }
-  }
-`;
-
 function Button(props) {
   const [email] = useState('brianna-workman@lambdastudents.com');
   const [showToast, setShowToast] = useState(false);
@@ -90,7 +30,7 @@ function Button(props) {
           Get in Touch
         </button>
       )}
-      {showToast && <Snackbar>{toastText}</Snackbar>}
+      {showToast && <div className='snackbar'>{toastText}</div>}
     </div>
   );
 }
