b2ca91a295db7c2a2b2900a9541a9c72876e574d
Brianna Workman <brianna.workman@hotmail.com>
2020-09-06 18:02:51 -0400

    sets up card and projects files

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 //component imports
 import Header from './components/header/Header';
+import Projects from './components/projects/Projects';
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <Projects />
     </div>
   );
 }
---
+++ src/components/projects/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from './card/Card';
+
+const Projects = () => {
+  return (
+    <div>
+      <Card />
+    </div>
+  );
+};
+
+export default Projects;
