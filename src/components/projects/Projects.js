23dc1303b07edb2d75070137cbc95bc5e5573f2d
Brianna Workman <brianna.workman@hotmail.com>
2020-09-07 16:58:31 -0400

    changes some spacing between the header and projects, uploads new github profile gif

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Card from './card/Card';
 import projectInfo from '../../assets/projectInfo';
+import './projects.scss';
 
 const Projects = () => {
   return (
-    <div>
-      {projectInfo.map((project) => {
-        return <Card key={project.id} projectInfo={project} />;
-      })}
+    <div className='project-container'>
+      <h2>My Projects</h2>
+      <div className='projects'>
+        {projectInfo.map((project) => {
+          return <Card key={project.id} projectInfo={project} />;
+        })}
+      </div>
     </div>
   );
 };
