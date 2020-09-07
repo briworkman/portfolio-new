4980f62623358cba0d7f911ee0f3e8fb342eb939
Brianna Workman <brianna.workman@hotmail.com>
2020-09-07 14:21:35 -0400

    cleans up image console logs

--- src/components/projects/image/Image.js
+++ src/components/projects/image/Image.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './image.scss';
 
 const Image = ({ source }) => {
-  console.log(source);
   return (
     <div className='project-gif'>
       <img src={source} alt='Project Gif' />
