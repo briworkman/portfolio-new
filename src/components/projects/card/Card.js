fdf68531c677ab8b6d3d7f4ae900fa6a958c2180
Brianna Workman <brianna.workman@hotmail.com>
2020-09-08 19:17:10 -0400

    renders and sets up links for github and live view

--- src/components/projects/card/Card.js
+++ src/components/projects/card/Card.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './card.scss';
 import Image from '../image/Image';
+import github from '../../../assets/images/github-brands.svg';
+import launch from '../../../assets/images/rocket-launch-solid.svg';
 
 const Card = ({ projectInfo }) => {
   console.log(projectInfo);
@@ -8,6 +10,22 @@ const Card = ({ projectInfo }) => {
     <div className='card-container'>
       <Image source={projectInfo.img.map((img) => img)} />
       <h1>{projectInfo.title}</h1>
+      <div className='icon-links'>
+        <a
+          href={projectInfo.github_url}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <img src={github} alt='View GitHub Repository' />
+        </a>
+        <a
+          href={projectInfo.live_url}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <img src={launch} alt='View Live' />
+        </a>
+      </div>
     </div>
   );
 };
